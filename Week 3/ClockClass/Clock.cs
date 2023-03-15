95c6bd8be602745d710e7a5e6402d5063b0a0473
MarcoGiacoppo <97724774+MarcoGiacoppo@users.noreply.github.com>
2023-03-16 00:30:34 +1100

    Add files via upload

---
+++ Week 3/ClockClass/Clock.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClockClass
+{
+    public class Clock
+    {
+        Counter _secs = new Counter("secs");
+        Counter _mins = new Counter("mins");
+        Counter _hrs = new Counter("hrs");
+        private string _name;
+
+        public Counter Seconds
+        {
+            get
+            {
+                return _secs;
+            }
+        }
+        public Counter Minutes
+        {
+            get
+            {
+                return _mins;
+            }
+        }
+        public Counter Hours
+        {
+            get
+            {
+                return _hrs;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+        public void Tick()
+        {
+            _secs.Increment();
+            if(_secs.Ticks > 59)
+            {
+                _mins.Increment();
+                _secs.Reset();
+            }
+            if(_mins.Ticks > 59)
+            {
+                _hrs.Increment();
+                _mins.Reset();
+            }
+            if (_hrs.Ticks > 23)
+            {
+                _hrs.Reset();
+                _mins.Reset();
+                _secs.Reset();
+            }
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine("{0}:{1}:{2}", _hrs.Ticks.ToString("00"), _mins.Ticks.ToString("00"), _secs.Ticks.ToString("00"));
+        }
+    }
+}
